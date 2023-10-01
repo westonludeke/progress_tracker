4b5382ba32a6be85d067ca751054f69f79beba98
westonludeke <westonludeke@gmail.com>
2023-10-01 15:53:01 -0500

    change backend port to 3003

--- backend/index.js
+++ backend/index.js
@@ -35,7 +35,7 @@ db.once('open', () => {
 });
 
 // Start the Express server
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3003;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
